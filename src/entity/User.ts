71d368f19781ccf3beb11d3a7065d71f4538e416
motojate <motojate@naver.com>
2023-11-25 17:23:54 +0900

    * USER 테이블 이름 변경

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,13 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity('USER')
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'user_seq' })
   userSeq: string;
 
   @Column({ nullable: true })
   name?: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, name: 'user_id' })
   userId: string;
 }
