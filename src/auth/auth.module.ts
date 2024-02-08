527e9dee5fe6e2fd5736c4ed36cdf28481dca144
motojate <motojate@naver.com>
2024-02-08 23:08:43 +0900

    * 토큰 만료시간 증가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { RedisCacheModule } from 'src/shared/redis/redis-cache.module';
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1m' },
+      signOptions: { expiresIn: '1000m' },
     }),
     UserModule,
     EmailVerificationModule,
