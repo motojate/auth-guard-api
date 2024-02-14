8dadacd666c4ec0eb976002f7dadc634fb44a035
motojate <motojate@naver.com>
2024-02-14 23:49:56 +0900

    * api로 변경

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpMemberUserDto } from './dtos/user.dto';
-import { Payload } from '@nestjs/microservices';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('create')
-  async signUp(@Payload() singUpMemberUser: SignUpMemberUserDto) {
-    console.log('동작한다');
+  async signUp(@Body() singUpMemberUser: SignUpMemberUserDto) {
     return this.userService.create(singUpMemberUser);
   }
 }
