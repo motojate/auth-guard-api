dc479da1060715469ae40d1afd90a6e6dd2753ba
motojate <motojate@naver.com>
2024-02-14 23:49:25 +0900

    * 카프카에서 api 서비스로 변경한다

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpMemberUserDto } from './dtos/user.dto';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Payload } from '@nestjs/microservices';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // @EventPattern('create_user')
+  @Post('create')
   async signUp(@Payload() singUpMemberUser: SignUpMemberUserDto) {
     console.log('동작한다');
     return this.userService.create(singUpMemberUser);
