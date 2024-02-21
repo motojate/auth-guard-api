8ccfb91e653bce6bc43795108080ba6f7213b359
motojate <motojate@naver.com>
2024-02-21 23:37:42 +0900

    * 프리즈마 로그 전략 변경

--- src/shared/prisma/prisma.service.ts
+++ src/shared/prisma/prisma.service.ts
@@ -1,23 +1,45 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    super({
-      log: ['query', 'error', 'info', 'warn'],
+    const loggingOptions = {
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    } satisfies Prisma.PrismaClientOptions;
+    super(loggingOptions);
+
+    this.$on('query', (e) => {
+      console.log(' ');
+      console.log('\x1b[34mQuery: \x1b[0m' + e.query);
+      console.log('\x1b[32mParams: \x1b[0m' + e.params);
+      console.log('\x1b[33m%s\x1b[0m', 'Duration: ' + e.duration + 'ms');
     });
-    this.$use(async (params, next) => {
-      const before = Date.now();
-      const result = await next(params);
-      const after = Date.now();
-      console.log(
-        `Query ${params.model}.${params.action} took ${after - before}ms`,
-      );
-      return result;
+    this.$on('error', async (e) => {
+      console.log(' ');
+      console.log('\x1b[31mMessage: \x1b[0m' + e.message);
+      console.log('Target: ' + e.target);
+      console.log('\x1b[33m%s\x1b[0m', 'TiemStamp: ' + e.timestamp);
     });
   }
 
