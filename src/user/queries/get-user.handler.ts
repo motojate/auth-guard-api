8b71e4f0d9a845b18dbb77cb40dcd54098a372a4
motojate <motojate@naver.com>
2024-02-28 19:08:01 +0900

    * [Query] 유저 조회 기능

---
+++ src/user/queries/get-user.handler.ts
@@ -0,0 +1,21 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetUserQuery } from './get-user.query';
+import { UserSiteMapping } from '@prisma/client';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
+
+@QueryHandler(GetUserQuery)
+export class GetUserHandler implements IQueryHandler<GetUserQuery> {
+  constructor(private readonly prisma: PrismaService) {}
+
+  execute(query: GetUserQuery): Promise<UserSiteMapping> {
+    return this.prisma.userSiteMapping.findUnique({
+      where: {
+        userSiteAuthProvider: {
+          userId: query.userId,
+          siteName: query.siteName,
+          authProvider: query.authProvider,
+        },
+      },
+    });
+  }
+}
