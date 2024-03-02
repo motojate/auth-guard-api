7b09dae65a97f5258d8166f2ebcdc5a21ab2e528
motojate <motojate@naver.com>
2024-03-02 23:48:18 +0900

    * 유저 모듈에 유저 생성 커맨드 프로바이더로 주입

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,17 @@ import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { RedisCacheModule } from 'src/shared/redis/redis-cache.module';
 import { GetValidUserIDHandler } from './queries/get-valid-user-id.handler';
+import { UserCreateHandler } from './commands/user-create.handler';
 
 @Module({
   imports: [RedisCacheModule],
-  providers: [UserService, AuthService, JwtService, GetValidUserIDHandler],
+  providers: [
+    UserService,
+    AuthService,
+    JwtService,
+    GetValidUserIDHandler,
+    UserCreateHandler,
+  ],
   controllers: [UserController],
   exports: [UserService],
 })
