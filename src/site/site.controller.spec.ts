0788b0272c34d3b25bae14910596b4943cfb88f0
motojate <motojate@naver.com>
2024-08-14 09:42:41 +0900

    [FEAT] site.controller 간단한 테스트 코스 뼈대 작성

--- src/site/site.controller.spec.ts
+++ src/site/site.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SiteController } from './site.controller';
+import { CommandBus } from '@nestjs/cqrs';
 
 describe('SiteController', () => {
   let controller: SiteController;
@@ -7,6 +8,14 @@ describe('SiteController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SiteController],
+      providers: [
+        {
+          provide: CommandBus,
+          useValue: {
+            execute: jest.fn()
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<SiteController>(SiteController);
